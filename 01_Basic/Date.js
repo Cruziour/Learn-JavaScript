38ade96c9f1fba02b4bfc1acaa853f2e84c3809f
Cruziour <164758339+Cruziour@users.noreply.github.com>
2024-04-27 17:38:21 +0000

    Date

---
+++ 01_Basic/Date.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleDateString("en-IN"));
+// console.log(typeof myDate);
+
+// let mycreatedDate = new Date(2023,0,23)
+// console.log(mycreatedDate.toDateString());
+let mycreatedDate = new Date(2023,0,23,5,3)
+
+// console.log(mycreatedDate.toLocaleDateString());
+console.log(mycreatedDate.getTime());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myTimeStamp);
