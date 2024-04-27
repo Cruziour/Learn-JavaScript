a0a4a4d4664d4fac91bd527f6ec239d442d832b5
Cruziour <164758339+Cruziour@users.noreply.github.com>
2024-04-27 17:24:00 +0000

    Number_Math

---
+++ 01_Basic/Num_Math.js
@@ -0,0 +1,42 @@
+// Number 
+// const num = 4000
+// console.log(num);
+
+// const score = new Number(400)
+// console.log(score);
+
+// const balance = new Number(500)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// // const otherNum = 1123.8966
+// const otherNum = 123.8966
+// console.log(otherNum.toPrecision(3));
+
+// const hundred = 10000000
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+// Maths
+// console.log(Math);
+// console.log(Math.abs(-57));
+// console.log(Math.round(5.5));
+// console.log(Math.round(5.2));
+// console.log(Math.round(5.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,0,8,1,-1,-8,2));
+// console.log(Math.max(4,0,8,1,-1,-8,2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
