12123a2f59ec609afcdb799baeeb451484a2b512
Cruziour <164758339+Cruziour@users.noreply.github.com>
2024-04-28 15:55:01 +0000

    Array part-1

--- 01_Basic/Date.js
+++ 01_Basic/Date.js
@@ -11,11 +11,28 @@ let myDate = new Date()
 
 // let mycreatedDate = new Date(2023,0,23)
 // console.log(mycreatedDate.toDateString());
-let mycreatedDate = new Date(2023,0,23,5,3)
+// let mycreatedDate = new Date(2023,0,23,5,3)
+let mycreatedDate = new Date("01-04-2025")
+// console.log(mycreatedDate.toDateString());
 
 // console.log(mycreatedDate.toLocaleDateString());
-console.log(mycreatedDate.getTime());
+// console.log(mycreatedDate.getTime());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myTimeStamp);
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay()+1);
+// console.log(newDate.getTime());
+
+// console.log(`${}`);
+
+let customiseDate = newDate.toLocaleString('default',{
+    weekday: "long"
+})
+console.log(customiseDate);
