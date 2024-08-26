1f814ad9eff044abc9a0f829961b286e29b62286
Rupesh Kumar Raushan <164758339+Cruziour@users.noreply.github.com>
2024-08-26 07:50:48 +0000

    conversion of data types

---
+++ 01_Basic/03_conversionOperator.js
@@ -0,0 +1,30 @@
+// let score = "33abx"
+// let score = "33"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber); // Nan = number
+
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" = > Nan 
+// true => 1;
+// false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
