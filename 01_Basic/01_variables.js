cfa15ca393a5d15f31f6797d03463b1b64cc25ef
Rupesh Kumar Raushan <164758339+Cruziour@users.noreply.github.com>
2024-08-26 07:15:57 +0000

    Variables

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "rupeshkumarraushan0@gmail.com"
+var accountPassword = "123456"
+accountCity = "Hajipur"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "rupeshkr99000@gmail.com"
+accountPassword = "21212121"
+accountCity = "Patna"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
