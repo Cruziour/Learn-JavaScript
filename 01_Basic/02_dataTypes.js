2d87a180c1778c5b9d26d4d113e91ca5d4c997c4
Rupesh Kumar Raushan <164758339+Cruziour@users.noreply.github.com>
2024-08-26 07:39:02 +0000

    Data Types

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+// console.log(3+3) 
+// console.log("Rupesh");
+
+// <~~~~~~~~~~~~~~~~~~Data Types~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let name = "Rupesh"
+let age = 21    
+let isLoggedIn = false
+let state = null
+
+// primitive
+//number => 2 to power 53
+// bigint
+// string = ""  ''
+// boolean
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+// console.log(typeof "Rupesh");
+// console.log(typeof(age));
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
