3aebfc299b998db935002901b4872475802541c7
Rupesh Kumar Raushan <164758339+Cruziour@users.noreply.github.com>
2024-08-31 04:54:24 +0000

    While & doWhile Loops

---
+++ 04_Loops/02_whileLoop.js
@@ -0,0 +1,28 @@
+// while (condition) {
+    
+// }
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = ['Ironman', 'Thor', 'Captain','Rocket','Spider-man']
+// let arr= 0
+// while (arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr++
+// }
+
+// do {
+    
+// } while (condition);
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
